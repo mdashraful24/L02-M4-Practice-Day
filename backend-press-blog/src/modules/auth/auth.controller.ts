a893d4b6071f7b35f10e0caeb1d7d36746bdd59b
mdashraful24 <mdashrafulislam2882@gmail.com>
2026-06-26 02:32:36 +0600

    added generated access token using refresh token

--- backend-press-blog/src/modules/auth/auth.controller.ts
+++ backend-press-blog/src/modules/auth/auth.controller.ts
@@ -9,16 +9,16 @@ const loginUser = catchAsync(async (req, res) => {
     const { accessToken, refreshToken } = result;
 
     res.cookie("accessToken", accessToken, {
+        secure: false,      // * In production => true
         httpOnly: true,
-        secure: false,
-        sameSite: "lax",
+        sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24
     });
 
     res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
         secure: false,
-        sameSite: "lax",
+        httpOnly: true,
+        sameSite: "none",
         maxAge: 1000 * 60 * 60 * 24 * 7
     });
 
@@ -30,8 +30,19 @@ const loginUser = catchAsync(async (req, res) => {
     });
 });
 
+const authRefreshToken = catchAsync(async (req, res) => {
+    const result = await authService.generateRefreshToken(req.cookies.refreshToken);
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Access token generated successfully",
+        data: result
+    });
+});
+
 
 export const authController = {
     loginUser,
-
+    authRefreshToken
 }
--- backend-press-blog/src/modules/auth/auth.service.ts
+++ backend-press-blog/src/modules/auth/auth.service.ts
@@ -5,7 +5,7 @@ import { IUser } from "../user/user.interface"
 import bcrypt from 'bcryptjs';
 import { jwtUtils } from '../../utils/jwt';
 import config from '../../config';
-import { SignOptions } from 'jsonwebtoken';
+import { JwtPayload, SignOptions } from 'jsonwebtoken';
 
 const loginUserIntoDB = async (payload: IUser) => {
     const { email, password } = payload;
@@ -57,8 +57,54 @@ const loginUserIntoDB = async (payload: IUser) => {
     };
 };
 
+const generateRefreshToken = async (token: string) => {
+
+    if (!token) {
+        throw new SelfErrorHandler("You are not logged in. Please log in to access this resource.", httpStatus.UNAUTHORIZED);
+    }
+
+    const verifiedToken = jwtUtils.verifyToken(token, config.jwt.refreshSecret);
+
+    if (!verifiedToken.success) {
+        throw new SelfErrorHandler(verifiedToken.error);
+    }
+
+    const { id, name, email, role } = verifiedToken.data as JwtPayload;
+
+    if (!role) {
+        throw new SelfErrorHandler("Forbidden. You don't have permission to access this resource.", httpStatus.FORBIDDEN);
+    }
+
+    const user = await prisma.user.findUnique({
+        where: { id, name, email, role }
+    });
+
+    if (!user) {
+        throw new SelfErrorHandler("User not found. Please log in again.", httpStatus.BAD_REQUEST);
+    }
+
+    if (user.activeStatus === "BLOCKED") {
+        throw new SelfErrorHandler("Your account has been blocked. Please contact support.", httpStatus.FORBIDDEN);
+    }
+
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    };
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt.accessSecret,
+        config.jwt.accessExpiresIn as SignOptions
+    );
+
+    return { accessToken };
+};
+
 
 export const authService = {
     loginUserIntoDB,
-
-}
+    generateRefreshToken
+};
